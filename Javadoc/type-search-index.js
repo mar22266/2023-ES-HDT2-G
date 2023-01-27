63510d1a0a4920768b69e0e552334d820e06dcf4
Andre <andremt2003@gmail.com>
2023-01-27 12:36:40 -0600

    javad

---
+++ Javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Controladora"},{"p":"<Unnamed>","l":"IPostfixCalculator"},{"p":"<Unnamed>","l":"IStack"},{"p":"<Unnamed>","l":"Pcalculator"},{"p":"<Unnamed>","l":"Principal"},{"p":"<Unnamed>","l":"StackUsingArrayList"}];updateSearchResults();
