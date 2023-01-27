bed8d6e618c73e54b61c492bec0240b7380bce30
Andre <andremt2003@gmail.com>
2023-01-26 22:35:05 -0600

    pRUEBAS PASADAS

--- Test/PcalculatorTest.java
+++ Test/PcalculatorTest.java
@@ -1,35 +1,42 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PcalculatorTest {
 
-        void testSuma() {
-            Pcalculator calc = new Pcalculator();
-            int a = 5;
-            int b = 5;
-            int resultado = calc.suma(a, b);
-            assertEquals(10, resultado);
-        }
+    @Test
+    void suma() {
+        Pcalculator calc = new Pcalculator();
+        int a = 5;
+        int b = 5;
+        int resultado = calc.suma(a, b);
+        assertEquals(10, resultado);
+    }
 
-        void testResta() {
-            Pcalculator calc = new Pcalculator();
-            int a = 5;
-            int b = 5;
-            int resultado = calc.resta(a, b);
-            assertEquals(0, resultado);
-        }
-        void testMultiplicacion() {
-            Pcalculator calc = new Pcalculator();
-            int a = 5;
-            int b = 5;
-            int resultado = calc.multiplicacion(a, b);
-            assertEquals(25, resultado);
-        }
+    @Test
+    void resta() {
+        Pcalculator calc = new Pcalculator();
+        int a = 5;
+        int b = 5;
+        int resultado = calc.resta(a, b);
+        assertEquals(0, resultado);
+    }
 
-        void testDivision() {
-            Pcalculator calc = new Pcalculator();
-            int a = 5;
-            int b = 5;
-            int resultado = calc.division(a, b);
-            assertEquals(1, resultado);
-        }
+    @Test
+    void multiplicacion() {
+        Pcalculator calc = new Pcalculator();
+        int a = 5;
+        int b = 5;
+        int resultado = calc.multiplicacion(a, b);
+        assertEquals(25, resultado);
+    }
+
+    @Test
+    void division() {
+        Pcalculator calc = new Pcalculator();
+        int a = 5;
+        int b = 5;
+        int resultado = calc.division(a, b);
+        assertEquals(1, resultado);
+    }
 }
